124b9c98a914ef7e38a442fa7f177b45fd00dd76
Aryan Walia <aryan.3walia@gmail.com>
2021-10-21 17:02:14 +0530

    Auth Work Done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(){
+        echo "login Successful";
+    }
+
+    public function signup(){
+        echo "signup Successful";
+    }
+}
